b6c97f0be86d703a443977bbfea91a680e110bd9
Somebody32x2 <samuelshuster@gmail.com>
2024-08-25 15:28:40 -0400

    Add Double Pendulum Simulation

---
+++ Assets/DoublePendulums/DoublePendulum.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using UnityEngine;
+
+public class DoublePendulum
+{
+    private double r1;
+    private double r2;
+    private double m1;
+    private double m2;
+    private double a1;
+    private double a2;
+    private double a1_v;
+    private double a2_v;
+    public bool dead;
+
+    public DoublePendulum(float length1, float length2, float mass1, float mass2, float angle1, float angle2)
+    {
+        this.r1 = length1;
+        this.r2 = length2;
+        this.m1 = mass1;
+        this.m2 = mass2;
+        this.a1 = Mathf.PI / angle1;
+        this.a2 = Mathf.PI / angle2;
+        this.a2_v = 0d;
+        this.a1_v = 0d;
+    }
+
+    public Vector3[] Update(float g, float speed, float dt)
+    {
+        // Log each step and its ingredients to show where NaNs are coming from
+        double num1 = -g * (2.0d * m1 + m2) * (a1);
+        // Debug.Log($"num1: {num1} m1: {m1} m2: {m2} a1: {a1} gravity: {g}");
+        double num2 = -m2 * g * Math.Sin(a1 - 2.0d * a2);
+        // Debug.Log($"num2: {num2} a1: {a1} a2: {a2}");
+        double num3 = -2.0d * Math.Sin(a1 - a2) * m2;
+        // Debug.Log($"num3: {num3} m2: {m2} a1: {a1} a2: {a2}");
+        double num4 = a2_v * a2_v * r2 + a1_v * a1_v * r1 * (a1 - a2);
+        // Debug.Log($"num4: {num4} a2_v: {a2_v} r2: {r2} a1_v: {a1_v} r1: {r1} a1: {a1} a2: {a2}");
+        double den = r1 * (2.0d * m1 + m2 - m2 * (2.0d * a1 - 2.0d * a2));
+        // Debug.Log($"den: {den} r1: {r1} m1: {m1} m2: {m2} a1: {a1} a2: {a2}");
+        double a1_a = (num1 + num2 + num3 * num4) / den;
+        
+        // Debug.Log($"a1_v: {a1_v} a2_v {a2_v} a1_a: {a1_a} \n num1: {num1} num2: {num2} num3: {num3} num4: {num4} den: {den}");
+
+        num1 = 2.0d * Math.Sin(a1 - a2);
+        num2 = a1_v * a1_v * r1 * (m1 + m2);
+        num3 = g * (m1 + m2) * Math.Cos(a1);
+        num4 = a2_v * a2_v * r2 * m2 * Math.Cos(a1 - a2);
+        den = r2 * (2.0d * m1 + m2 - m2 * Math.Cos(2.0d * a1 - 2.0d * a2));
+        double a2_a = (num1 * (num2 + num3 + num4)) / den;
+
+        // Debug.Log($"a1_v: {a1_v} a2_v {a2_v} a1_a: {a1_a}, a2_a: {a2_a}");
+        
+        double x1 = r1 * Math.Sin(a1);
+        double y1 = r1 * Math.Cos(a1);
+        
+        double x2 = x1 + r2 * Math.Sin(a2);
+        double y2 = y1 + r2 * Math.Cos(a2);
+        
+        
+        // Debug.Log($"a1_v {a1_v} a2_v {a2_v} a1 {a1} a2 {a2} a1_a {a1_a} a2_a {a2_a} speed {speed} dt {dt}");
+        this.a1_v += a1_a * speed * dt;
+        this.a2_v += a2_a * speed * dt;
+        this.a1 += a1_v * speed * dt;
+        this.a2 += a2_v * speed * dt;
+        // Debug.Log($"a1_v {a1_v} a2_v {a2_v} a1 {a1} a2 {a2} a1_a {a1_a} a2_a {a2_a} speed {speed} dt {dt}");
+        
+        // Detect NaNs
+        if (!dead && double.IsNaN(x1) || double.IsNaN(y1) || double.IsInfinity(x1) || double.IsInfinity(y1) || double.IsNaN(x2) || double.IsNaN(y2) || double.IsInfinity(x2) || double.IsInfinity(y2))
+        {
+            // Debug.LogWarning("Particle died");
+            dead = true;
+            m1 = 0;
+            m2 = 0;
+            r1 = 0;
+            r2 = 0;
+            x1 = 0;
+            y1 = 0;
+            a1 = 0;
+            a2 = 0;
+            a1_v = 0;
+            a2_v = 0;
+            a1_a = 0;
+            a2_a = 0;
+            x2 = 0;
+            y2 = 0;
+        }
+        
+        return new Vector3[]
+        {
+            new Vector3((float)x1, (float)y1, 0),
+            new Vector3((float)x2, (float)y2, 0)
+        };
+    }
+}
---
+++ Assets/DoublePendulums/DoublePendulumsManager.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class DoublePendulumsManager : MonoBehaviour
+{
+
+    private ParticleSystem ps;
+    public GameObject UI;
+    public GameObject dtReferenceVar;
+
+    public float mass1 = 50;
+    public float mass2 = 50;
+    public float length1 = 1;
+    public float length2 = 1;
+    public float spread1 = 1f;
+    public float spread2 = 0f;
+    public float gravity = 1f;
+    private List<DoublePendulum> pendulums = new List<DoublePendulum>();
+    public int numPendulums = 200;
+    public float speed = 1; // Speed of the simulation -- Directly multiplies velocity calculations
+    public float updatesPerTick = 1; // Provides a way to modify speed without changing velocity calculations (except if multByDt)
+    public float magnification = 100;
+    public bool showPoint1 = true;
+    public bool showPoint2 = true;
+    public bool drawLines = true;
+    public bool multByDt = true; // Multiply the speed by the delta time, or use 1/60
+    public LineRenderer lineRenderer;
+    
+    public float userTrailLength = 0.0001f;
+    // public bool useZ = false;
+    
+    private bool lastDrawLines = true;
+    private int lastNumPendulums = 200;
+    private float lastUserTrailLength = 0.0001f;
+    private bool lastShowPoint1 = true;
+    private bool lastShowPoint2 = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastShowPoint1 = showPoint1;
+        lastShowPoint2 = showPoint2;
+        gameObject.GetComponent<LineRenderer>().enabled = drawLines;
+        if (drawLines) lineRenderer = gameObject.GetComponent<LineRenderer>();
+        
+        ps = GetComponent<ParticleSystem>();
+        SetNumPendulums(numPendulums.ToString());
+        StartCoroutine(SetColors());
+        ResetPS();
+        // ResetPendulums();   
+        
+    }
+
+    public float updatesRemaining = 0;
+    private void Update()
+    {
+        if (lastNumPendulums != numPendulums)
+        {
+            SetNumPendulums(numPendulums+"");
+        }
+        if (lastUserTrailLength != userTrailLength)
+        {
+            SetTrailLength(userTrailLength+"");
+        }
+        if (lastShowPoint1 != showPoint1 || lastShowPoint2 != showPoint2)
+        {
+            ResetPS(false);
+            lastShowPoint1 = showPoint1;
+            lastShowPoint2 = showPoint2;
+        }
+
+        updatesRemaining += updatesPerTick;
+        float dt = multByDt ? Time.deltaTime : 1 / 60f;
+        while (updatesRemaining >= 1)
+        {
+            updatesRemaining--;
+
+            // Update the position of each particle in the system
+            ParticleSystem.Particle[] particles =
+                new ParticleSystem.Particle[(numPendulums) * ((showPoint1 ? 1 : 0) + (showPoint2 ? 1 : 0))];
+            int numParticlesAlive = ps.GetParticles(particles);
+
+            for (int i = 0; i < pendulums.Count; i++)
+            {
+                // if (i * (showPoint2 ? 2 : 1) >= particles.Length - 1) 
+                // Debug.Log($"len {pendulums.Count} i {i} showPoint1 {showPoint1} showPoint2 {showPoint2} particles.Length {particles.Length} i * (showPoint2 ? 2 : 1) {i * (showPoint2 ? 2 : 1)}");
+                Vector3[] positions = pendulums[i].Update(gravity, speed, dt);
+                if (showPoint1)
+                {
+                    if (updatesRemaining <= 1) particles[i * (showPoint2 ? 2 : 1)].position = positions[0] * magnification;
+                }
+
+                if (showPoint2)
+                {
+                    if (updatesRemaining <= 1) particles[i * (showPoint1 ? 2 : 1) + (showPoint1 ? 1 : 0)].position = positions[1] * magnification;
+                }
+            }
+            if (updatesRemaining <= 1) ps.SetParticles(particles, numParticlesAlive);
+        }
+
+        // Apply the particle changes to the Particle System
+        
+        
+        // On Press of i/k increase/decrease speed by one
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (speed < 1) speed += 0.1f;
+            else speed++;
+            SetSpeed(speed.ToString(), true);
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            if (speed <= 1) speed -= 0.1f;
+            else speed--;
+            SetSpeed(speed.ToString(), true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            SetSpeed("0", true);
+        }
+        if (drawLines)
+        {
+            RedrawLines();
+        }
+        if (lastDrawLines != drawLines)
+        {
+            gameObject.GetComponent<LineRenderer>().enabled = drawLines;
+            lastDrawLines = drawLines;
+        }
+    }
+
+    private void ResetPendulums()
+    {
+        pendulums = new List<DoublePendulum>();
+        for (int i = 0; i < numPendulums; i++)
+        {
+            pendulums.Add(new DoublePendulum(length1, length2, mass1, mass2, -2f + spread1 / numPendulums * i, -2f + spread2 / numPendulums * i));
+        }
+    }
+
+    public void ResetPS(bool resetPends=true)
+    {
+        if (resetPends) ResetPendulums();
+        // Reset the particle system
+        ps.Clear();
+        ps.Play();
+        StartCoroutine(SetColors());
+    }
+
+    private void RedrawLines()
+    {
+        var main = ps.main;
+        lineRenderer.positionCount = main.maxParticles;
+        var particles = new ParticleSystem.Particle[main.maxParticles];
+        ps.GetParticles(particles);
+        var positions = particles.Select(particle => particle.position).ToArray();
+        // Insert (0, 0, 0) so that it always goes after a point2 if point1 is enabled
+        
+        if (showPoint1 && showPoint2)
+        {
+            List<Vector3> npos = new List<Vector3>();
+            npos.Capacity = positions.Length * 4;
+            for (int i = 0; i < positions.Length; i += 2)
+            {
+                npos.Add(Vector3.zero);
+                npos.Add(positions[i]);
+                npos.Add(positions[i + 1]);
+                npos.Add(positions[i]);
+                
+            }
+            positions = npos.ToArray();
+        }
+        else
+        {
+            List<Vector3> npos = new List<Vector3>();
+            npos.Capacity = positions.Length * 2;
+            for (int i = 0; i < positions.Length; i++)
+            {
+                npos.Add(Vector3.zero);
+                npos.Add(positions[i]);
+            }
+            positions = npos.ToArray();
+        }
+        // Debug.Log(positions.Length + " " + lineRenderer.positionCount + " " + positions[0] + " " + positions[^1] + " " + particles.Length);
+        lineRenderer.SetPositions(positions);
+    }
+
+    public void ResetTime()
+    {
+        ResetPS();
+    }
+    public void SetNumPendulums(string inumPendulums)
+    {
+        numPendulums = int.Parse(inumPendulums);
+        lastNumPendulums = numPendulums;
+        // Set the number of pendulums
+        var main = ps.main;
+        main.maxParticles = (Int32.Parse(inumPendulums) + 1) * ((showPoint1 ? 1 : 0) + (showPoint2 ? 1 : 0));
+        Debug.Log($"max particles {(Int32.Parse(inumPendulums) + 1) * ((showPoint1 ? 1 : 0) + (showPoint2 ? 1 : 0))}");
+        var burst = ps.emission.GetBurst(0);
+        burst.count = (Int32.Parse(inumPendulums) + 1) * ((showPoint1 ? 1 : 0) + (showPoint2 ? 1 : 0));
+        ps.emission.SetBurst(0, burst);
+        ResetPS();
+    }
+    public void SetTrailLength(string trailLength)
+    {
+        lastUserTrailLength = userTrailLength;
+        // Set the trail length of the particle system (THIS GETS CALLED BY THE UI COMPONENT)
+        var trail = ps.trails;
+        userTrailLength = float.Parse(trailLength);
+        trail.lifetime = userTrailLength;
+        ResetPS();
+        ResetPS();
+    }
+    public void SetTrailLength(string trailLength, bool resetPS)
+    {
+        // Set the trail length of the particle system
+        var trail = ps.trails;
+        trail.lifetime = float.Parse(trailLength);
+        if (resetPS) ResetPS();
+        if (resetPS) ResetPS();
+    }
+    public void SetSpeed(string speedStr)
+    {
+        // Set the speed
+        speed = float.Parse(speedStr);
+        // if (speed == 0) SetTrailLength("0", false);
+        // else SetTrailLength(userTrailLength + "", false);
+        // SetTrailLength(Mathf.Abs(userTrailLength/speed).ToString(), false);
+        // ResetPS();
+    }
+    public void SetSpeed(string speedStr, bool setUI=false)
+    {
+        // Set the speed
+        speed = float.Parse(speedStr);
+        if (speed == 0) SetTrailLength("0", false);
+        else SetTrailLength(userTrailLength + "", false);
+        // SetTrailLength(Mathf.Abs(ps.trails.lifetime.constant/speed).ToString(), false);
+        // ResetPS();
+        if (setUI)
+        {
+            dtReferenceVar.GetComponentInChildren<TMP_InputField>().text = speedStr;
+        }
+    }
+
+    // public void ResetToDefaults()
+    // {
+    //     // Set the current attractor to its default values
+    //     CircleAttractor attractor = attractors[attractorDropdownNameIndexes[attractorDropdown.value]];
+    //     attractor.variables = attractor.DefaultVariables;
+    //     // Set the speed to the default value
+    //     speed = 1;
+    //     // Reset the UI
+    //     SwitchAttractor(attractorDropdown.value);
+    // }
+
+    IEnumerator SetColors()
+    {
+        yield return new WaitForSeconds(0.05f);
+        // Give each particle its own rainbow color by looping through each particle and setting its color
+        ParticleSystem.Particle[] particles = new ParticleSystem.Particle[ps.main.maxParticles];
+        int numParticlesAlive = ps.GetParticles(particles);
+        for (int i = 0; i < numParticlesAlive; i++)
+        {
+            float t = (float)i / numParticlesAlive;
+            particles[i].startColor = Color.HSVToRGB(t, 1, 1);
+        }
+        ps.SetParticles(particles, numParticlesAlive);
+    }
+}
